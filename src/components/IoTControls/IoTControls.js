bc37cdd22be0adf7dd0cc037b5c79f093c283ca1
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-19 21:32:19 -0700

    edit iot

--- src/components/IoTControls/IoTControls.js
+++ src/components/IoTControls/IoTControls.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import './iotcontrols.css'
-
+import React from "react";
+import "./iotcontrols.css";
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
 
 const IoTControls = () => {
-
+  const iconStyle = {
+    fontSize:'70px'
+  }
   return (
     <section id="iotControls">
-    <div className="iotControls-main" className="about__content">
-    <div className="about__cards">
-      <article className='about__card'>
-        {/* <FaAward className='about__icon'/> */}
-        <h5>Lights</h5>
-      </article>
-      <article className='about__card'>
-        {/* <FaAward className='about__icon'/> */}
-        <h5>Temperature</h5>
-      </article>
-      <article className='about__card'>
-        {/* <FiUser className='about__icon'/> */}
-        <h5>Lock</h5>
-      </article>
-      <article className='about__card'>
-        {/* <VscFolderLibrary className='about__icon'/> */}
-        <h5>Other</h5>
-      </article>
-    </div>
-    </div>
+      <div className="iotControls-main">
+        <article className="control__card">
+          <LightbulbIcon style={iconStyle}/>
+          <h5>Lights</h5>
+        </article>
+        <article className="control__card">
+        <ThermostatIcon style={iconStyle}/>
+          <h5>Temperature</h5>
+        </article>
+        <article className="control__card">
+          {/* <FiUser className='about__icon'/> */}
+          <h5>Lock</h5>
+        </article>
+        <article className="control__card">
+          {/* <VscFolderLibrary className='about__icon'/> */}
+          <h5>Other</h5>
+        </article>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default IoTControls
+export default IoTControls;
